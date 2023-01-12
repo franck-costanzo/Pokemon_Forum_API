579e1572b0f7775255f3be1992c1040c107fc820
Franck-Costanzo <franck.costanzo@laplateforme.io>
2023-01-12 12:45:56 +0100

    LikesController completed

---
+++ backend/Controllers/LikesController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pokemon_Forum_API.DTO.LikeDTO;
+using Pokemon_Forum_API.Entities;
+using Pokemon_Forum_API.Services;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+using utils;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pokemon_Forum_API.Controllers
+{
+    [ApiController]
+    [Route("/likes")]
+    public class LikesController : ControllerBase
+    {
+        string connectionString = Utils.ConnectionString;
+        LikeService likeService = new LikeService();
+
+        public LikesController(){}
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Likes>> GetLikeById(int id)
+        {
+            var like = await likeService.GetLikeById(connectionString, id);
+            if (like != null)
+            {
+                return Ok(like);
+            }
+            else
+            {
+                return BadRequest("An error occurred while getting like. Please check your request and try again.");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Likes>> PostLike(LikeDto like)
+        {
+            try
+            {
+                var createdLike = await likeService.CreateLike(connectionString, like);
+                if (createdLike != null)
+                {
+
+                    return Ok(createdLike);
+                }
+                else
+                {
+                    return BadRequest("An error occurred while creating the like. Please check your request and try again.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("An error occurred while creating the like. Please check your request and try again.");
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Likes>> DeleteLike(int id)
+        {
+            var deletedLike = await likeService.DeleteLike(connectionString, id);
+            if (deletedLike != null)
+            {                
+                return Ok(deletedLike);
+            }
+            else
+            {
+                return BadRequest("An error occurred while deleting the like. Please check your request and try again.");
+            }
+            
+        }
+
+
+    }
+}
