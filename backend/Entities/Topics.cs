4805308a0d67e4c02f9843a9ddfb305c46258a34
Franck-Costanzo <franck.costanzo@laplateforme.io>
2023-01-13 10:15:34 +0100

    Entity, DTO and service for Topics created

---
+++ backend/Entities/Topics.cs
@@ -0,0 +1,30 @@
+﻿namespace Pokemon_Forum_API.Entities
+{
+    public class Topics
+    {
+        #region Properties
+
+        public int topic_id { get; set; }
+
+        public string name { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Topics() { }
+
+        public Topics(int topic_id, string name) 
+        { 
+            this.topic_id = topic_id;
+            this.name = name;
+        }
+
+        public Topics(string name) 
+        { 
+            this.name = name;
+        }
+
+        #endregion
+    }
+}
